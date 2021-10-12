02c6258fe04ad1a23110e275cefc2a2006a4916d
Daniel <rossidesofficial@gmail.com>
2021-10-13 00:01:52 +0100

    Updated familyTree.js

--- familyTree/familyTree.js
+++ familyTree/familyTree.js
@@ -1,25 +1,40 @@
+// Create class for making royal family objects.
 class Person {
-    constructor(name=String, mother=String, father=String) {
-        this.name = name
-        this.parents = [mother, father]
-        this.childOf = function () {
-            return `${mother} & ${father}`
-        }
-    }
-}
-
-
-familyTree = [new Person("King George VI", "", ""),
-new Person("Queen Elizabeth", "", ""),
-new Person("Prince Philip", "", ""),
-new Person("Queen Elizabeth II", "Queen Elizabeth", "King George VI"),
-new Person("Princess Margaret", "Queen Elizabeth", "King George VI"),
-new Person("Camila", "", ""),
-new Person("Charles", "Queen Elizabeth II", "Prince Philip"),
-new Person("Diana", "", ""),
-new Person("Anne", "Queen Elizabeth II", "Prince Philip"),
-new Person("Prince Andrew", "Queen Elizabeth II", "Prince Philip"),
-new Person("Prince Edward", "Queen Elizabeth II", "Prince Philip")]
-
-
-console.log(familyTree[0])
+    // Constructor contains two parameters.
+    constructor(name, parents) {
+        // Two attributes are made from these parameters.
+        this.name = name;
+        this.parents = parents;
+    };
+    // Class method attempts to join .parents attribute into a string, else retuns "No known parents".
+    childOf = () => this.parents.map((parent) => parent.name).join(" & ") || "No known parents";
+};
+// First generation array is created with two Person objects.
+const gen1 = [
+    new Person("King George VI", []),
+    new Person("Queen Elizabeth", []),
+];
+// Adding Parents to Par1 array.
+const Par1 = [
+    gen1.find((person) => person.name === "King George VI"),
+    gen1.find((person) => person.name === "Queen Elizabeth"),
+];
+// Second generation array is created with three Person objects.
+const gen2 = [
+    new Person("Prince Philip", []),
+    new Person("Queen Elizabeth II", Par1),
+    new Person("Princess Margaret", Par1),
+];
+// Adding Parents to Par2 array.
+const Par2 = [
+    gen2.find((person) => person.name === "Prince Philip"),
+    gen2.find((person) => person.name === "Queen Elizabeth II"),
+];
+// Third generation array is created with five Person objects.
+const gen3 = [
+    new Person("Camila", []),
+    new Person("Charles", Par2),
+    new Person("Diana", []),
+    new Person("Anne", Par2),
+    new Person("Prince Edward", Par2),
+];
